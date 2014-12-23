76129525680187720542889f38a705c91660bdac
Jameson Aranda <jamesona@users.noreply.github.com>
2014-12-23 11:37:11 -0700

    Update dominfo.sh

--- shell/dominfo.sh
+++ shell/dominfo.sh
@@ -1,32 +1,17 @@
 #!/bin/bash
-
-# Copyright (c) 2014 Jameson Aranda
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
-# and associated documentation files (the "Software"), to deal in the Software without restriction, 
-# including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
-# subject to the following conditions:
-# The above copyright notice and this permission notice shall be included in all copies or substantial
-# portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
-# LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
-# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH 
-# THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-# This script requires a compiled binary "pageranksum" to be added to your executable path, or included
-# in the directory that this script is executed from. The original source document is not available
-# from the source site, but has been included in this repository. 
-# To compile, run from the download directory:  gcc -o pageranksum pagerank.c
-
-# initiate loop over input args
+# Copyright (c) 2014 Jameson Aranda Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
+# following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. This script requires a compiled binary "pageranksum" to be
+# added to your executable path, or included in the directory that this script is executed from. The original source document is not available from the source site, but has been included in this repository. To compile, run from
+# the download directory: gcc -o pageranksum pagerank.c initiate loop over input args
 ###############################
 for dom; do
 
   # Parse domain
   ##############
-  dom=$(sed 's/^h\?t\?t\?p\?s\?:\?\/\?\/\?//' <<< $dom | sed 's/\/.*$//' | rev | cut -d"." -f1-2 | rev)
+  dom=$(sed 's/^\(https\|http\):\?\/\?\/\?//' <<< $dom | sed 's/\/.*$//' | rev | cut -d"." -f1-2 | rev)
 
   # Get domain registration date
   ##############################
@@ -34,7 +19,9 @@ for dom; do
 
   # Get indexed links (parsed from google search result page)
   ###########################################################
-  linkindex=$(curl -A Mozilla -s --get --data-urlencode "q=site:$dom" http://www.google.ca/search | egrep -o '<div class="sd" id="resultStats">[^<]+</div>' | sed 's/<\/\?[^>]\+>//g' | sed 's/[a-zA-Z ]//g')
+  linkindex=$(curl -A Mozilla -s --get --data-urlencode "q=site:$dom" http://www.google.ca/search)
+  linkindex=$(egrep -o '<div class="sd" id="resultStats">[^<]+</div>' <<< $linkindex)
+  linkindex=$( sed 's/<\/\?[^>]\+>//g' <<< $linkindex | sed 's/[a-zA-Z ]//g')
 
   # Get backlinks
   ###############
@@ -42,16 +29,18 @@ for dom; do
 
   # Get PageRank
   ##############
-  pagerank=$(curl -A Mozilla -s --get  "http://toolbarqueries.google.com/tbr?client=navclient-auto&ch=`pageranksum $dom`&features=Rank&q=info:$dom" | cut -d":" -f3)
+  request="http://toolbarqueries.google.com/tbr?client=navclient-auto&ch=`pageranksum $dom`&features=Rank&q=info:$dom"
+  pagerank=$(curl -A Mozilla -s --get "$request" | cut -d":" -f3)
   if [ $pagerank = '' ]; then pagerank="N/A"; else pagerank="$pagerank/10"; fi
 
   # Echo out result
   #################
-  echo "===  $dom  ==="
-  echo "Created: $created"
-  echo "Indexed Links: $linkindex"
-  echo "Backlinks: $backlinks"
-  echo "PageRank: $pagerank"
-  
-done
+  echo
+  echo -e "===\t$dom\t==="
+  echo -e "Created:\t$created"
+  echo -e "Indexed Links:\t$linkindex"
+  echo -e "Backlinks:\t$backlinks"
+  echo -e "PageRank:\t$pagerank"
+  echo
 
+done
