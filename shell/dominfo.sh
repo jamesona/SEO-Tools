fcbc706d4f660eff778b710b89573248aa1f3717
Jameson Aranda <jamesona@users.noreply.github.com>
2015-01-27 11:48:31 -0700

    Update dominfo.sh

--- shell/dominfo.sh
+++ shell/dominfo.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
-# Copyright (c) 2014 Jameson Aranda Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction,
-# including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
-# following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. This script requires a compiled binary "pageranksum" to be
-# added to your executable path, or included in the directory that this script is executed from. The original source document is not available from the source site, but has been included in this repository. To compile, run from
-# the download directory: gcc -o pageranksum pagerank.c initiate loop over input args
+# License
+##########
+# Copyright (c) 2014 Jameson Aranda Permission is hereby granted, free of charge, to any person obtaining a copy 
+# of this software and associated documentation files (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following 
+# conditions: The above copyright notice and this permission notice shall be included in all copies or substantial 
+# portions of the Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN 
+# AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR 
+# OTHER DEALINGS IN THE SOFTWARE. 
+
+# Info
+#######
+# This script requires a compiled binary "pageranksum" to be added to your executable path, 
+# or included in the directory that this script is executed from. The original source document
+# for the binary is not available from the source site, but has been included in this repository. 
+# To compile, run the following from the download directory: 
+# gcc -o pageranksum pagerank.c 
+
+
+# Initiate loop over input args
 ###############################
 for dom; do
 
