a5bb3451794c7c0b2c8234cf9c37405571e4a213
Jameson Aranda <jamesona@users.noreply.github.com>
2015-03-17 19:19:08 -0600

    Update loader.js
    adding app.js injector

--- bookmarklets/bobCalendar/loader.js
+++ bookmarklets/bobCalendar/loader.js
@@ -2,3 +2,7 @@
   var url = 'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js';
   document.body.appendChild(document.createElement('script')).src=url;
 })();
+(function(){
+  var url = 'https://cdn.rawgit.com/jamesona/SEO-Tools/Book-Of-Business/bookmarklets/bobCalendar/app.js';
+  document.body.appendChild(document.createElement('script')).src=url;
+})();
