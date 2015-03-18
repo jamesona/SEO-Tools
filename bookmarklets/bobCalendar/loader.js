00f8f6b499129f325075d181294a1edffc10fb1e
Jameson Aranda <jamesona@users.noreply.github.com>
2015-03-18 08:35:14 -0600

    Update loader.js

--- bookmarklets/bobCalendar/loader.js
+++ bookmarklets/bobCalendar/loader.js
@@ -1,10 +1,10 @@
 var body = document.getElementsByTagName('body')[0]; body.setAttribute('ng-app', 'calendar');
 (function(){
   var url = 'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js';
-  document.body.appendChild(document.createElement('script')).src=url;
+  document.head.appendChild(document.createElement('script')).src=url;
 })();
 (function(){
   var url = 'https://rawgit.com/jamesona/SEO-Tools/Book-Of-Business/bookmarklets/bobCalendar/app.js';
-  document.body.appendChild(document.createElement('script')).src=url;
+  document.head.appendChild(document.createElement('script')).src=url;
 })();
 body.innerHTML += "<p>{{'this is a '+'string'}}</p>";
