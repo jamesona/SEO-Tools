ea5b76eea6460d289b65b4afb9fd469da71baf0f
Jameson Aranda <jamesona@users.noreply.github.com>
2015-03-18 08:38:09 -0600

    Update loader.js

--- bookmarklets/bobCalendar/loader.js
+++ bookmarklets/bobCalendar/loader.js
@@ -1,6 +1,6 @@
 var body = document.getElementsByTagName('body')[0]; body.setAttribute('ng-app', 'calendar');
 (function(){
-  var url = 'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js';
+  var url = 'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.js';
   document.head.appendChild(document.createElement('script')).src=url;
 })();
 (function(){
