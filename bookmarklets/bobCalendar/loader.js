f9d32949235fced17cf70000a1b8c017087c4dcd
Jameson Aranda <jamesona@users.noreply.github.com>
2015-03-17 19:09:55 -0600

    Update loader.js
    Cleaning up.

--- bookmarklets/bobCalendar/loader.js
+++ bookmarklets/bobCalendar/loader.js
@@ -1,1 +1,4 @@
-(function(){document.body.appendChild(document.createElement('script')).src='https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js';})();
+(function(){
+  var url = 'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js';
+  document.body.appendChild(document.createElement('script')).src=url;
+})();
