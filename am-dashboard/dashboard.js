3b996eb2238688f25b7a20a050ebd95e428e1fe9
Jameson Aranda <jamesona@users.noreply.github.com>
2015-04-27 16:33:32 -0600

    Create dashboard.js

---
+++ am-dashboard/dashboard.js
@@ -0,0 +1,6 @@
+/* call using the following enclosure:
+(function(){
+  var url = 'https://rawgit.com/jamesona/SEO-Tools/am-dashboard/dashboard.js';
+  document.head.appendChild(document.createElement('script')).src=url;
+})();
+*/
