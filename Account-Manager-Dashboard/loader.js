ce1526357819ee101647881535f3a7d6295bfa21
Jameson Aranda <jamesona@users.noreply.github.com>
2015-04-27 16:25:54 -0600

    Update loader.js

--- Account-Manager-Dashboard/loader.js
+++ Account-Manager-Dashboard/loader.js
@@ -1,1 +1,7 @@
 
+/* call using the following enclosure:
+(function(){
+  var url = 'https://rawgit.com/jamesona/SEO-Tools/master/Account-Manager-Dashboard/loader.js';
+  document.head.appendChild(document.createElement('script')).src=url;
+})();
+*/
