c88a2329b21bf29150b5c420a925eb66c0366a23
Jameson Aranda <jamesona@users.noreply.github.com>
2015-04-29 10:21:50 -0600

    Update dashboard.js

--- am-dashboard/dashboard.js
+++ am-dashboard/dashboard.js
@@ -207,13 +207,14 @@ function Dashboard() {
 			for (var i=0;i<tickets.length;i++){
 				var ticket = tickets[i],
 				listItem = document.createElement('li'),
-				text = ticket.CompanyName+': '+ticket.TicketTypeName
-				text += ' (Due: '+(ticket.ScheduledEndDate.getMonth()+1)+
-				text += '/'+ticket.ScheduledEndDate.getDate()+'/'
-				text += ticket.ScheduledEndDate.getFullYear()+')'
-				listItem.innerHTML = text.
+				text = ticket.CompanyName+': '+ticket.TicketTypeName;
+				text += ' (Due: '+(ticket.ScheduledEndDate.getMonth()+1);
+				text += '/'+ticket.ScheduledEndDate.getDate()+'/';
+				text += ticket.ScheduledEndDate.getFullYear()+')';
+				listItem.innerHTML = text;
 				list.appendChild(listItem);
 			}
+			return list;
 		},
 	}
 }
