ee2ac8d070527c794ce53e23e53e6205b2d9fd3d
Jameson Aranda <jamesona@users.noreply.github.com>
2015-04-29 08:47:15 -0600

    Update dashboard.js

--- am-dashboard/dashboard.js
+++ am-dashboard/dashboard.js
@@ -164,7 +164,7 @@ function Dashboard() {
 		},
 		getCritical: function() {
 			var tickets = this.getTickets(),
-			    critical = [];
+			critical = [];
 			for (var i = 0; i < tickets.length; i++) {
 				if (tickets[i].ScheduledEndDate < new Date()) {
 					critical.push(tickets[i]);
@@ -174,8 +174,8 @@ function Dashboard() {
 		},
 		sortTickets: function(){
 			var data = [];
+			if (! self.Tickets.ticketArray) this.getTickets();
 			for (var i=0;i<db.Tickets.ticketArray.length;i++){
-				if (! self.Tickets.ticketArray) this.getTickets();
 				var ticket = self.Tickets.ticketArray[i],
 				date = new Date(ticket.ScheduledEndDate),
 				day = date.getDate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear();
