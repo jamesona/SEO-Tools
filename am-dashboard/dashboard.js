8185d2856e4b427ce486c5548fd1cf960ede0a47
Jameson Aranda <jamesona@users.noreply.github.com>
2015-04-30 15:08:44 -0600

    Update dashboard.js

--- am-dashboard/dashboard.js
+++ am-dashboard/dashboard.js
@@ -313,23 +313,23 @@ function Dashboard(application, ko, bootbox) {
     rel: 'stylesheet',
     type: 'text/css',
     href: 'https://rawgit.com/jamesona/SEO-Tools/master/am-dashboard/dashboard.css',
-  }),
+  });
   this.HTML.nav = this.Tools.createElement(this.HTML.dashboard, 'ul', {
     className: 'nav navbar-nav',
-  }),
+  });
   this.HTML.tools = this.Tools.createElement(this.HTML.nav, 'li', {
     innerHTML: '<a class="dropdown-toggle" data-toggle="dropdown">Tools</a>',
     className: 'dropup',
-  }),
+  });
   this.HTML.toolsMenu = this.Tools.createElement(this.HTML.tools, 'ul', {
     className: 'dropdown-menu',
-  }),
+  });
   this.HTML.todoistExport = this.Tools.createElement(this.HTML.toolsMenu, 'li', {
     innerHTML: '<a>Todoist Export</a>',
     onclick: function(){
       self.Tools.todoistExport();
     },
-  }),
+  });
   this.HTML.bobCalendar = this.Tools.createElement(this.HTML.toolsMenu, 'li', {
     innerHTML: '<a>BoB Calendar</a>',
     onclick: function(){
@@ -343,24 +343,30 @@ function Dashboard(application, ko, bootbox) {
       data = self.Tools.bobParse(prompt('Paste BoB data')),
       calendar = new self.Tickets.calendar(data);
       calendar.draw(modal);
-    }
-  }),
+    },
+  });
   this.HTML.tickets = this.Tools.createElement(this.HTML.nav, 'li', {
     innerHTML: '<a class="dropdown-toggle" data-toggle="dropdown">Tickets</a>',
     className: 'dropup',
-  }),
+  });
   this.HTML.ticketsMenu = this.Tools.createElement(this.HTML.tickets, 'ul', {
     className: 'dropdown-menu',
-  }),
+  });
   this.HTML.ticketCalendar = this.Tools.createElement(this.HTML.ticketsMenu, 'li', {
     innerHTML: '<a>Calendar</a>',
     onclick: function(){
-      var node = bootbox.alert('none')[0].children[0].children[0].children[0],
+      bootbox.dialog({
+        message: " ",
+        title: "",
+        className: "big-modal",
+        buttons: {},
+      });
+      var modals = document.getElementsByClassName('bootbox-body'), modal = modals[modals.length-1],
       data = self.Tickets.sortTickets(),
       calendar = new self.Tickets.calendar(data);
-      calendar.draw(node);
-    }
-  }),
+      calendar.draw(modal);
+    },
+  });
   this.HTML.critical = this.Tools.createElement(this.HTML.nav, 'li', {
     innerHTML: self.Tickets.countCritical(),
     id: 'critical',
